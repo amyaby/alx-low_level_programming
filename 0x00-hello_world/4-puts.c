83b4d739998e37181e57ff938f5c4311710191c8
amyaby <imane4abas@gmail.com>
2023-06-15 16:46:04 -0700

    don't forget comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-int main() 
+
+/**
+ * main - entry function
+ * Description: printing a message
+ * Return: 0 if exited properly
+ */
+
+int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 return(0);
