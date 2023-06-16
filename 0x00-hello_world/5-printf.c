feb7c407000b3b1007439e38950d056be13b6751
amyaby <imane4abas@gmail.com>
2023-06-15 17:07:29 -0700

    print the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - entry function
+* Description: print a message but now using printf function
+* Return: 0 if exited properly
+*/
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
