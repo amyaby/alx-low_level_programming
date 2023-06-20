ec7bada1bcc04e046a5a549d4065cd29fc01d8a1
amyaby <imane4abas@gmail.com>
2023-06-20 12:36:09 -0700

    spaces

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry function
+ * Description:program that prints the alphabet, in lowercase
+ *, followed by a new line.
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+for (ch = a ; ch <= z ; ch++)
+{
+		for (i = 0 ; i <= 10 ; i++)
+		{
+		_putchar(ch);
+}
+	_putchar("\n");
+	}
+return (0);
+
