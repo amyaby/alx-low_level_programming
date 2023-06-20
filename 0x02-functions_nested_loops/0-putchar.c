3d0fbe53779af2316e2d85fe0eb9ce79594c00b5
amyaby <imane4abas@gmail.com>
2023-06-20 10:26:03 -0700

    do not forget to add ur function to main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,12 @@
 */
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 }
