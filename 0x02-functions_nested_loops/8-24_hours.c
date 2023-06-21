3964d4b1997b09613141eb800998bed028d4f82f
amyaby <imane4abas@gmail.com>
2023-06-21 02:31:55 -0700

    print every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+	for (min = 0; min < 60; min++)
+	{
+		_putchar((hr / 10) + 48);
+		_putchar((hr % 10) + 48);
+		_putchar(':');
+		_putchar((min / 10) + 48);
+		_putchar((min % 10) + 48);
+		_putchar('\n');
+	}
+	}
+}
