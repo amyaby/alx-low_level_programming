0dad9d6df3e9edec0aa06188bc78053e3e7318a3
amyaby <imane4abas@gmail.com>
2023-06-22 18:14:00 -0700

    no print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,13 +6,23 @@
  */
 void print_diagonal(int n)
 {
-	if (n >= 0)
+	if (n <= 0)
 	{
-		_putchar(' ');
-_puchar('\\');
+		_putchar('\n');
 	} else
 	{
-_putchar('\n');
+		int x, y;
+
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < n; y++)
+			{
+				if (y == x)
+					_putchar('\\');
+				else if (y < x)
+					_putchar(' ');
 			}
 			_putchar('\n');
 		}
+	}
+}
