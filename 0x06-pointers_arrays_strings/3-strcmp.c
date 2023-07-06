a218c0d424d08fd59a1e3ad213cac8a123f36688
amyaby <imane4abas@gmail.com>
2023-07-06 16:41:11 -0700

    compare two strings with strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ *  @s2: second string to compare
+ * Return: result of the comparison
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
