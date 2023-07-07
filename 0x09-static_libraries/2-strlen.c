7083f04008dfcb59721f59f4c8db1d64059a1dab
amyaby <imane4abas@gmail.com>
2023-07-07 09:15:29 -0700

    create a library

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,2 +1,21 @@
-int _strlen(char *s);
+#include "main.h"
 
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
