009742f8af49f9425da22328ff0110a72b2a9312
amyaby <imane4abas@gmail.com>
2023-07-11 14:24:34 -0700

    free a grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,18 +1,20 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
-* free_grid -> removing memory allocation
-* @grid: grid to be freed
-* @height: height of matrix
-* Return: nothing
+* free_grid  - frees a 2 dimensional grid previously created.
+* @grid: the grid to free.
+* @height: height of the grid.
 */
+
 void free_grid(int **grid, int height)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < height; i++)
+	while (i < height)
+	{
 		free(grid[i]);
-	free(grid);
+		i++;
+	}
+		free(grid);
 }
