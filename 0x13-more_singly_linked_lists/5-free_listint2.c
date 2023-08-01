20bd6c0a4637e37e36afc387afdd5f051d1c3e9b
amyaby <imane4abas@gmail.com>
2023-08-01 06:00:50 -0700

    feeing a list meth 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - function that frees a singly linked list.
+ * @head: pointer to pointer to the head of linked list.
+ *
+ * Return: No return.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		ptr =  *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+}
