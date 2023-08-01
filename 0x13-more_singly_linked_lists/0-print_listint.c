d8dee5af656923ee2ece5b44939c27ab6fe5d0c9
amyaby <imane4abas@gmail.com>
2023-07-31 18:50:21 -0700

    print the data of a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+  
+ /**
+ * print_listint - printing the data of a linked list
+ * @h: the head pointer
+ *
+ * Return: number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+const listint_t *p = h;
+size_t count = 0; 
+if (p == NULL)
+{
+printf("linked list is empty\n");
+return (0);
+}
+while (p != NULL)
+{
+printf("%d\n", p->n);
+p = p->next;
+count++;
+}
+printf("\n");
+return (count);
+}
