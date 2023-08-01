2adc14b2f8082355f10df8418feb1182c2dd5c6d
amyaby <imane4abas@gmail.com>
2023-07-31 18:40:00 -0700

    print the data of a list

--- 0-print_listint.c
+++ 0-print_listint.c
@@ -10,24 +10,23 @@
 
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *p = h;
+const listint_t *p = h;
 
-    size_t count = 0;
+size_t count = 0;
 
-    if (p == NULL)
-    {
-        printf("linked list is empty\n");
-        return 0;
-    }
+if (p == NULL)
+{
+printf("linked list is empty\n");
+return (0);
+}
+while (p != NULL)
+{
+printf("%d\n", p->n);
+p = p->next;
+count++;
+}
 
-    while (p != NULL)
-    {
-        printf("%d\n", p->n);
-        p = p->next;
-        count++;
-    }
-
-    printf("\n");
-    return count;
+printf("\n");
+return (count);
 }
 
