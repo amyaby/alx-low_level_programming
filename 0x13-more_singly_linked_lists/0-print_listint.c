dbeba54d9b9d7fd4672d1905c506768851c07df3
amyaby <imane4abas@gmail.com>
2023-07-31 19:18:48 -0700

    print the data of nodes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,28 +1,21 @@
-#include <stdio.h>
 #include "lists.h"
 
- /**
- * print_listint - printing the data of a linked list
- * @h: linked list
+/**
+ * print_listint - prints all the data of a linked list
+ * @h: the head link pointer
  *
  * Return: number of nodes
-*/
-
+ */
 size_t print_listint(const listint_t *h)
 {
-const listint_t *p = h;
-size_t count = 0;
-if (p == NULL)
-{
-printf("linked list is empty\n");
-return (0);
-}
-while (p != NULL)
-{
-printf("%d\n", p->n);
-p = p->next;
-count++;
-}
-printf("\n");
-return (count);
+	size_t node = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		node++;
+		h = h->next;
+	}
+
+	return (node);
 }
