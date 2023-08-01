838110350b61a9e871752700cc8bc26bffef5e2f
amyaby <imane4abas@gmail.com>
2023-08-01 07:42:21 -0700

    no delete node

---
+++ 10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ * @head: pointer
+ * @index: index of the node t
+ * Return: 1 (Success), -1 (Fail)
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *t = *head;
+	listint_t *c = NULL;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(t);
+		return (1);
+	}
+
+	while (i < index - 1)
+	{
+		if (!t || !(t->next))
+			return (-1);
+		t = t->next;
+		i++;
+	}
+
+
+	c = t->next;
+	t->next = c->next;
+	free(c);
+
+	return (1);
+}
